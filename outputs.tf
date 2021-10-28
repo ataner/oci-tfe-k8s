5bf7d4a63e674b23cfd7c32f803a0220af18552b
Renata Rocha <renata.rocha@slalom.com>
2021-10-28 19:30:55 -0400

    removing outputs that dont work

--- outputs.tf
+++ outputs.tf
@@ -7,13 +7,3 @@ output "nodepool_ids" {
   description = "Map of Nodepool names and IDs"
   value       = module.oke.nodepool_ids
 }
-
-output "ssh_to_operator" {
-  description = "convenient command to ssh to the operator host"
-  value       = "ssh -i ${var.ssh_private_key_path} -J opc@${local.bastion_public_ip} opc@${local.operator_private_ip}"
-}
-
-output "ssh_to_bastion" {
-  description = "convenient command to ssh to the bastion host"
-  value       = "ssh -i ${var.ssh_private_key_path} opc@${local.bastion_public_ip}"
-}
